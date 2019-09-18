11e7ebd6e1cad02480390e2b718e2d0a99ca907f
Sergei <sdonich@gmail.com>
2019-09-18 10:04:16 +0200

    Update script.js during lecture

--- script.js
+++ script.js
@@ -1,131 +1,64 @@
 const BASE_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-// class GoodsItem {
-//   constructor(id, title = 'No name', price = 'No price') {
-//     this.id = id;
-//     this.title = title;
-//     this.price = price;
-//   }
-//   render() {
-//     return `<div class="goods-item" >
-//       <h3>${this.title}</h3>
-//       <p>${this.price}</p>
-//       <button data-id="${this.id}">Добавить в корзину</button>
-//     </div>`;
-//   }
-// }
-//
-// class GoodsList {
-//   constructor(container = '.container') {
-//     this.container = container;
-//     this.goods = [];
-//     this.filteredGoods = [];
-//   }
-//   totalPrice() {
-//     return this.goods.reduce((total, good) => {
-//       if (!good.price) return total;
-//       return total += good.price;
-//     }, 0);
-//   }
-//   async fetchGoods() {
-//     try {
-//       this.goods = await makeGETRequest(`${BASE_URL}/catalogData.json`);
-//       this.filteredGoods = [...this.goods];
-//       console.log(this.goods);
-//       return this.goods;
-//     } catch (e) {
-//       console.error(e);
-//       throw new Error(e);
-//     }
-//   }
-//   filterGoods(value) {
-//     console.log(value);
-//     const regexp = new RegExp(value, 'i');
-//     this.filteredGoods = this.goods.filter((good) => {
-//       return regexp.test(good.product_name);
-//     });
-//     this.render();
-//   }
-//   addEvents(el = 'button', event = () => {}) {
-//     const container = document.querySelector(this.container);
-//     const elements = container.querySelectorAll(el);
-//     elements.forEach((currentEl) => {
-//       currentEl.addEventListener('click', event);
-//     })
-//   }
-//   render() {
-//     document.querySelector(this.container).innerHTML = this.filteredGoods.reduce((acc, item) => {
-//       const good = new GoodsItem(item.id_product, item.product_name, item.price);
-//       return acc += good.render();
-//     }, '');
-//   }
-// }
-//
-// class Cart extends GoodsList {
-//   async add(good) {
-//     try {
-//       const { result } = await makeGETRequest(`${BASE_URL}/addToBasket.json`);
-//       if (!result) {
-//         throw new Error('Ошибка добавления');
-//       }
-//       this.goods.push(good);
-//       console.log(this.goods);
-//     } catch (e) {
-//       throw new Error(e);
-//     }
-//   }
-//   remove(id) {
-//     if (!id) {
-//       // clean cart
-//       return;
-//     }
-//   }
-//   update(id, good) {}
-// }
-//
-// class CartItem extends GoodsItem {
-//   constructor(title = 'No name', price = 'No price', count = 1) {
-//     super(title, price);
-//     this.count = count;
-//   }
-// }
-//
-// const cart = new Cart();
-// const list = new GoodsList('.goods-list', cart);
-// const searchForm = document.querySelector('.goods-search-from');
-// const searchInput = document.querySelector('.goods-search');
-// searchForm.addEventListener('submit', (event) => {
-//   event.preventDefault();
-//   const value = searchInput.value;
-//   list.filterGoods(value);
-// });
-// list.fetchGoods().then(() => {
-//   list.render();
-//   list.addEvents('button', (event) => {
-//     const buttonEl = event.target;
-//     const goodId = buttonEl.getAttribute('data-id');
-//     const good = list.goods.find((currentGood) => {
-//       return currentGood.id_product == goodId;
-//     });
-//     cart.add(good);
-//   });
-// });
+Vue.component('goods-item', {
+  props: ['good'],
+  methods: {
+    add() {
+      this.$emit('add', this.good.id_product);
+    }
+  },
+  template: `
+    <div class="goods-item">
+      <h3>{{ good.product_name }}</h3>
+      <p>{{ good.price }}</p>
+      <button @click="add">Добавить в корзину</button>
+    </div>
+  `
+});
+
+Vue.component('goods-list', {
+  props: ['goods'],
+  methods: {
+    addTo(id_product) {
+      this.$emit('add', id_product);
+    }
+  },
+  template: `
+    <div class="goods-list">
+      <goods-item
+        v-for="good in goods"
+        :good="good"
+        :key="good.id_product"
+        @add="addTo"
+      >
+      </goods-item>
+    </div>
+  `
+});
 
 const app = new Vue({
   el: '#app',
   data: {
     goods: [],
-    filteredGoods: [],
     searchLine: '',
     isVisibleCart: false
+  },
+  computed: {
+    filteredGoods() {
+      const regexp = new RegExp(this.searchLine, 'i');
+
+      return this.goods.filter(good => regexp.test(good.product_name))
+    }
   },
   mounted() {
     this.makeGETRequest(`${BASE_URL}/catalogData.json`).then((goods) => {
       this.goods = goods;
-      this.filteredGoods = goods;
     }).catch(err => console.error(err));
   },
   methods: {
+    addToCart(id_product) {
+      console.log('add product', id_product)
+    },
     makeGETRequest(url) {
       return new Promise((resolve, reject) => {
         const xhr = window.XMLHttpRequest ? new window.XMLHttpRequest() : new window.ActiveXObject('Microsoft.XMLHTTP');
@@ -146,8 +79,8 @@ const app = new Vue({
         xhr.send();
       });
     },
-    onSearchSubmit(searchLine) {
-      this.filteredGoods = this.filterGoods(searchLine)
+    toggleCartVisibility() {
+      this.isVisibleCart = !this.isVisibleCart;
     },
     filterGoods(searchLine) {
       let regexp = new RegExp(`${searchLine}`, 'gi');
